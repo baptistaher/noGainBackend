1d179f0b01107f45fe9938b4dc829d66c3b13700
BH <baptistamhernani@gmail.com>
2026-02-20 15:49:44 +0000

    ci: update all package ci: add docker file for develope with pnpm

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testEnvironment: 'node',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': '@swc/jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/$1',
+  },
+  setupFiles: ['reflect-metadata'],
+
+  verbose: true,
+  coverageProvider: 'v8',
+  coveragePathIgnorePatterns: ['/node_modules/', '.interface.ts', '.enum.ts'],
+  clearMocks: true,
+};
+
+export default config;
--- src/models/Employee/index.ts
+++ src/models/Employee/index.ts
@@ -1,96 +1,96 @@
 // import { Employee } from './../../../node_modules/.prisma/client/index.d';
 
-import { hash } from "bcryptjs";
-import {
-  CreateEmployeeRequestDTO,
-  CreateEmployeeResponseDTO,
-} from "../../dto/Employee/Create";
-import { prisma } from "../../database/prismaClient";
 import { Roles } from "@prisma/client";
+import { hash } from "bcryptjs";
+import { prisma } from "../../database/prismaClient";
+import {
+	type CreateEmployeeRequestDTO,
+	CreateEmployeeResponseDTO,
+} from "../../dto/Employee/Create";
 
 export class Employee {
-  static async create(data: CreateEmployeeRequestDTO) {
-    try {
-      const hashedPassword = await hash("123456", 12);
-      const newEmployee = await prisma.user.create({
-        data: {
-          name: data.name,
-          email: data.email,
-          phone: data.phone,
-          password: hashedPassword,
-          role: Roles.EMPLOYEE,
-          cni: data.cni,
-          nif: data.nif,
-          birth: new Date(data.birth),
-          employee: {
-            create: {},
-          },
-        },
-      });
+	static async create(data: CreateEmployeeRequestDTO) {
+		try {
+			const hashedPassword = await hash("123456", 12);
+			const newEmployee = await prisma.user.create({
+				data: {
+					name: data.name,
+					email: data.email,
+					phone: data.phone,
+					password: hashedPassword,
+					role: Roles.EMPLOYEE,
+					cni: data.cni,
+					nif: data.nif,
+					birth: new Date(data.birth),
+					employee: {
+						create: {},
+					},
+				},
+			});
 
-      return newEmployee;
-    } catch (error) {
-      throw new Error("problem with db");
-    }
-  }
+			return newEmployee;
+		} catch (error) {
+			throw new Error("problem with db");
+		}
+	}
 
-  static async getAll() {
-    const data = await prisma.employee.findMany({
-      select: {
-        id: true,
-        user: {
-          select: {
-            name: true,
-            email: true,
-            phone: true,
-          },
-        },
-      },
-    });
+	static async getAll() {
+		const data = await prisma.employee.findMany({
+			select: {
+				id: true,
+				user: {
+					select: {
+						name: true,
+						email: true,
+						phone: true,
+					},
+				},
+			},
+		});
 
-    const employee = data.map((item) => {
-      return {
-        id: item.id,
-        name: item.user.name,
-        email: item.user.email,
-      };
-    });
+		const employee = data.map((item) => {
+			return {
+				id: item.id,
+				name: item.user.name,
+				email: item.user.email,
+			};
+		});
 
-    return employee;
-  }
+		return employee;
+	}
 
-  static async getById(id: string) {
-    const data = await prisma.employee.findUnique({
-      where: {
-        id,
-      },
-      select: {
-        user: {
-          select: {
-            name: true,
-            email: true,
-            birth: true,
-            phone: true,
-            cni: true,
-            nif: true,
-          },
-        },
-      },
-    });
+	static async getById(id: string) {
+		const data = await prisma.employee.findUnique({
+			where: {
+				id,
+			},
+			select: {
+				user: {
+					select: {
+						name: true,
+						email: true,
+						birth: true,
+						phone: true,
+						cni: true,
+						nif: true,
+					},
+				},
+			},
+		});
 
-    if (!data) {
-      throw new Error("Employee ID not found");
-    }
+		if (!data) {
+			throw new Error("Employee ID not found");
+		}
 
-    const employee = {
-      name: data.user.name,
-      email: data.user.email,
-      birth: data.user.birth.toISOString().slice(0, 10),
-      phone: data.user.phone,
-      cni: data.user.cni,
-      nif: data.user.nif,
-    };
+		const employee = {
+			name: data.user.name,
+			email: data.user.email,
+			birth: data.user.birth.toISOString().slice(0, 10),
+			phone: data.user.phone,
+			cni: data.user.cni,
+			nif: data.user.nif,
+		};
 
-    return employee;
-  }
+		return employee;
+	}
 }
