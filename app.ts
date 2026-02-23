3782ed72e8b013048388d0e09f94959899cee14d
BH <baptistamhernani@gmail.com>
2026-02-23 22:01:34 +0000

    feat: add pino and pinohttp with minimal config

--- app.ts
+++ app.ts
@@ -3,8 +3,11 @@ import bodyParser from "body-parser";
 import express from "express";
 import { router } from "./src/routes";
 import { Config } from "./src/config/config";
+import logger from "./src/logger/logger";
 
 const app = express();
+
+app.use(logger);
 
 app.use(express.json());
 app.use(bodyParser.json());
--- src/config/config.ts
+++ src/config/config.ts
@@ -20,11 +20,18 @@ const CONFIG_PRISMA_SCHEMA = object({
 	DATABASE_URL: string().nonempty("Database Url is required"),
 });
 
+const CONFIG_LOGGER_SCHEMA = object({
+	LOGGER_LEVEL: string().nonempty("Log Level is required"),
+});
+
 export const Config = {
 	prisma: CONFIG_PRISMA_SCHEMA.parse({
 		DATABASE_URL: process.env.DATABASE_URL,
 	}),
 	api: CONFIG_API_SCHEMA.parse({
 		API_PORT: process.env.API_PORT,
+	}),
+	logger: CONFIG_LOGGER_SCHEMA.parse({
+		LOGGER_LEVEL: process.env.LOGGER_LEVEL,
 	}),
 };
