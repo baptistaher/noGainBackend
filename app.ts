cabf972fb538b68fb042e5b69496c9bcf8a3607b
BH <baptistamhernani@gmail.com>
2026-03-07 12:16:39 -0100

    feat: add open telemetry but is not working ok i wii back on it in the future

--- app.ts
+++ app.ts
@@ -1,8 +1,8 @@
 import "./src/open-telemetry/instrumentation";
-import { trace } from "@opentelemetry/api";
+import {  trace } from "@opentelemetry/api";
 import bodyParser from "body-parser";
 import express from "express";
-
+import {  requestCounter, requestDuration } from "./src/open-telemetry/metrics";
 import { Config } from "./src/config/config";
 import logger from "./src/logger/logger";
 import { router } from "./src/routes";
@@ -16,10 +16,27 @@ app.use(logger);
 app.use(express.json());
 app.use(bodyParser.json());
 
+// --- Middleware to track metrics ---
+app.use((req, res, next) => {
+  const start = Date.now();
+  res.on("finish", () => {
+    const duration = Date.now() - start;
+    requestCounter.add(1, { route: req.path, method: req.method, status: res.statusCode });
+    requestDuration.record(duration, { route: req.path, method: req.method, status: res.statusCode });
+  });
+  next();
+});
+
+// const expressInstrumentation = new ExpressInstrumentation({
+// 	// ignoreLayersType: ["middleware"]
+// })
+
+// expressInstrumentation.setTracerProvider(trace);
+
 app.use("/api", router);
 
 app.listen(Config.api.API_PORT, () => {
-	const span = tracer.startSpan("starting-project");
+	const span = tracer.startSpan("server.start");
 	logger.logger.info(`server is runnig on port ${Config.api.API_PORT}`);
 
 	span.end();
--- src/open-telemetry/instrumentation.ts
+++ src/open-telemetry/instrumentation.ts
@@ -1,19 +1,21 @@
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
-import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-http";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
 import { resourceFromAttributes } from "@opentelemetry/resources";
-import { ConsoleMetricExporter, PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
+import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
 import { NodeSDK } from "@opentelemetry/sdk-node";
-import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-node";
+
 import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } from "@opentelemetry/semantic-conventions";
+import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-http";
 
 const traceExporter = new OTLPTraceExporter({
 	url: "http://otel-collector:4318/v1/traces",
 });
 
+
 const metricExporter = new OTLPMetricExporter({
 	url: "http://otel-collector:4318/v1/metrics",
 });
+
 
 const sdk = new NodeSDK({
 	resource: resourceFromAttributes({
